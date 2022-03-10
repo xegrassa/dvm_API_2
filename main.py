6fbf2bb132c8f11bb161c89d52b25e9be8009310
egrassa <bacek90@mail.ru>
2022-03-10 20:10:01 +0500

    - Добавлена функция для вывода общего кол-ва кликов по ссылку - Общий адресс Битли вынесен в константу - Добавлен перехват ошибок неправильно введенного url

--- main.py
+++ main.py
@@ -5,20 +5,36 @@ from urllib.parse import urlparse
 import requests
 from dotenv import load_dotenv
 
+BITLY_URL = 'https://api-ssl.bitly.com'
+
 
 def shorten_link(token, url):
-    URL = 'https://api-ssl.bitly.com/v4/bitlinks'
+    endpoint = f'{BITLY_URL}/v4/bitlinks'
     headers = {
         'Authorization': f'Bearer {token}'
     }
     payload = {
         'long_url': f'{url}'
     }
-    response = requests.post(URL, headers=headers, json=payload)
+    response = requests.post(endpoint, headers=headers, json=payload)
+    response.raise_for_status()
+    bitlink = response.json()['link']
+    parsed_link = urlparse(bitlink)
 
-    link = response.json()['link']
-    parsed_url = urlparse(link)
-    return parsed_url.netloc + parsed_url.path
+    return parsed_link.netloc + parsed_link.path
+
+
+def count_clicks(token, bitlink):
+    endpoint = f'{BITLY_URL}/v4/bitlinks/{bitlink}/clicks/summary'
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+    url_params = {
+        'units': '-1',
+    }
+    response = requests.get(endpoint, headers=headers, params=url_params)
+    response.raise_for_status()
+    return response.json()['total_clicks']
 
 
 def args_parse():
@@ -30,7 +46,21 @@ def args_parse():
 
 def main(url):
     tokken = os.getenv('TOKKEN')
-    print('Битлинк', shorten_link(tokken, url))
+    try:
+        bitlink = shorten_link(tokken, url)
+    except requests.exceptions.HTTPError:
+        print(f'Неправильно введенный URL {url}')
+        return
+    else:
+        print('Битлинк', bitlink)
+
+    try:
+        total_clicks = count_clicks(tokken, bitlink)
+    except requests.exceptions.HTTPError:
+        print(f'Неправильный Bitlink {bitlink}')
+        return
+    else:
+        print('Количество кликов', total_clicks)
 
 
 if __name__ == '__main__':
